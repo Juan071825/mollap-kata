093c6a67b95d070ac4b339bee6a5b9689249043f
juan071825 <juanmateoalvarez@gmail.com>
2026-02-28 18:07:27 +0100

    feat(programadorTasques.py): :sparkles: Progresar en clase ProgramadorTasques.

---
+++ administrador/programadorTasques.py
@@ -0,0 +1,16 @@
+from tareas import Tareas
+
+class ProgramadorTasques():
+
+    def __init__(self, target):
+        self.tasques = Tareas()
+        self.tasques.setTarget(target)
+
+    def getTarea():
+        pass
+
+    def setTarea():
+        pass
+
+    def ejecutarTareas(self):
+        self.tasques.ejecucion()
