259d6c1d7f6a9f42050d2f3c00a3737e4b839704
juan071825 <juanmateoalvarez@gmail.com>
2026-02-28 19:36:43 +0100

    fix(mollapp.py): :bug: Arreglar bugs.

--- clients/mollapp.py
+++ clients/mollapp.py
@@ -1,12 +1,14 @@
+from clients.cliente import Cliente
 from administrador.programadorTasques import ProgramadorTasques
 
-from cliente import Cliente
-
-class Mollapp(Cliente()):
+class Mollapp(Cliente):
 
     def __init__(self):
         self.programadorTareas = None
 
 
     def setProgramadorTasques(self, programadorTareas):
-        self.programadorTasques = programadorTareas
+        self.programadorTareas = programadorTareas
+
+    def enviarPeticion(self, peticion):
+        self.programadorTareas.ejecutarTareas(peticion)
