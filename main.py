35daa8f6f7bdad3c40f908fd80518263f8fce03a
juan071825 <juanmateoalvarez@gmail.com>
2026-02-28 19:39:16 +0100

    feat(main.py): :sparkles: Añadir main.

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+from targets.vehiculo import Vehiculo
+from filtres.autenticacion import Autenticacion
+from filtres.autorizacion import Autorizacion
+from administrador.programadorTasques import ProgramadorTasques
+from clients.mollapp import Mollapp
+
+
 def main():
-    print("Hello from mollap-kata!")
+
+
+    vehiculo = Vehiculo()
+
+
+    programador = ProgramadorTasques(vehiculo)
+
+
+    programador.setTarea(Autenticacion())
+    programador.setTarea(Autorizacion())
+
+
+    app = Mollapp()
+    app.setProgramadorTasques(programador)
+
+
+    app.enviarPeticion("Juan")
+
+    vehiculo.ejecucion("Juan")
 
 
 if __name__ == "__main__":
